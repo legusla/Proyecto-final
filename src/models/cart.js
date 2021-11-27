b210f28c5c4ee6339b7318698055c97d25b917b4
legusla <leonardoscarcella@yahoo.com>
2021-11-27 17:51:33 -0300

    actualizacion de nodemon y cambios en carrito

--- src/models/cart.js
+++ src/models/cart.js
@@ -22,16 +22,19 @@ const getProductsByIdCart = async (id) => {
 };
 
 //agrega un producto a un carrito por id
-const addProductsToCart = async (id, update) => { 
-    const cart = await cartContenedor.getById(id); 
+const addProductsToCart = async (id, idProduct) =>  {
+    const cart = await cartContenedor.getById(id);
     const { products } = cart;
-
-    products.filter(products => products.id != update);
-    const cartUpdated = await cartContenedor.update(update)
-    console.log(update)
-
+  
+    products.splice(idProduct, 1);
+  
+    const newCart = {
+      ...cart,
+      products
+    }
+    const cartUpdated = await cartContenedor.update(id, newCart);
     return cartUpdated;
-};
+  };
 
 //borra un producto de un carrito por id
 const deleteProductToCart = async (id, idProduct) => {
